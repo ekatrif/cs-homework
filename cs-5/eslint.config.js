50cb7f1fd67d0dfc7e01012f3639067c7b02ff7f
Ekaterina Trifonova <3420922@gmail.com>
2026-04-07 10:26:22 +0300

    feat: add prettier

--- cs-5/eslint.config.js
+++ cs-5/eslint.config.js
@@ -1,9 +1,11 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  prettierConfig, // Добавляем отключение правил, конфликтующих с Prettier
   {
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
@@ -16,23 +18,21 @@ export default tseslint.config(
   {
     rules: {
       'indent': ['error', 2],
-      'max-len': ['error', { code: 100, ignoreComments: true, ignoreStrings: true }],
       'quotes': ['error', 'single'],
       'semi': ['error', 'always'],
       'comma-dangle': ['error', 'always-multiline'],
       'space-before-function-paren': ['error', 'never'],
       'space-infix-ops': 'error',
       'keyword-spacing': ['error', { before: true, after: true }],
-      'semi': ['error', 'always'],
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
-    {
+  {
     files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
     ...tseslint.configs.disableTypeChecked,
   },
   {
     ignores: ['dist', 'node_modules', '**/*.config.js', '**/*.config.ts'],
-  },
+  }
 );
