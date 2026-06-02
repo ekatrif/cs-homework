7f02562570a6e4a6cb75f94b20c85b17fa99ebd9
Ekaterina Trifonova <3420922@gmail.com>
2026-06-02 09:31:48 +0300

    feat: add lastIndexOf function, refactoring

--- cs-14/functions.ts
+++ cs-14/functions.ts
@@ -1,67 +1,52 @@
-type ObjectType = Record<string, string | number>;
-type ValueType = string | number;
+type ObjectType = Record<string, unknown>;
 
-type ConditionFunction = (obj: ObjectType) => ValueType;
+type KeyFunction = (obj: ObjectType) => number;
 
-const searchInArray = (arr: ValueType[] | ObjectType[]): arr is ValueType[] => {
-  if (Array.isArray(arr) && arr.every(el => (typeof el === 'number' || typeof el === 'number') )) {
-      return true
+type SearchType = 'indexOf' | 'lastIndexOf';
+
+const searchInArrayOfNumbers = (arr: number[] | ObjectType[]): arr is number[] => {
+  if (Array.isArray(arr) && arr.every(el => (typeof el === 'number') )) {
+    return true
   }
   return false;
 }
 
-const valueIsNumber = (searchValue: ValueType): searchValue is number => {
-  return typeof searchValue === 'number';
-}
-
-const valueIs = (searchValue: ValueType): searchValue is number => {
-  return typeof searchValue === 'number';
-}
-
-function indexOf(arr: number[], value: number): number;
-function indexOf(arr: ObjectType[], value: ValueType, condFunc?: ConditionFunction): number;
-
-function indexOf(arr: number[] | ObjectType[], value: ValueType, condFunc?: ConditionFunction): number {
+function getIndex(arr: number[] | ObjectType[], target: number, searchType: SearchType, keyFunc?: KeyFunction): number {
   let left = 0;
   let right = arr.length - 1;
   let result = -1;
 
-  if (searchInArray(arr) && valueIsNumber(value)) {
-    while (left <= right) {
-      const mid = left + Math.floor((right - left) / 2);
-      const current = arr[mid];
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    const current = searchInArrayOfNumbers(arr) ? arr[mid] : keyFunc?.(arr[mid]);
 
-      if (current === value) {
-        result = mid;
-        right = mid - 1;
-      } else if (current < value) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
-    return result;
-  } else if (arr.every(el => typeof el === 'object') && condFunc) {
-    while (left <= right) {
-      const mid = left + Math.floor((right - left) / 2);
-      const current = arr[mid];
-      const valueToCompare = condFunc(current);
-
-      if (valueToCompare === value) {
-        result = mid;
-        right = mid - 1;
-      } else if (valueToCompare < value) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
+    if (current === undefined) {
+      throw new Error('keyFunc is undefined');
     }
 
-    return result;
+    if (current === target) {
+      result = mid;
+
+      if (searchType === 'indexOf') {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else if (current < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
   }
+  return result;
+}
 
-  return -1;
+function indexOf(arr: number[] | ObjectType[], target: number, keyFunc?: KeyFunction): number {
+  return getIndex(arr, target, 'indexOf', keyFunc);
+}
 
+function lastIndexOf(arr: number[] | ObjectType[], target: number, keyFunc?: KeyFunction): number {
+  return getIndex(arr, target, 'lastIndexOf', keyFunc);
 }
 
 const ages = [12, 42, 42, 42, 56];
@@ -73,4 +58,8 @@ const users = [
   { age: 56, name: 'Bill' }
 ];
 console.log(indexOf(ages, 42));
-console.log(indexOf(users, 42, (item) => item.age));
+console.log(indexOf(users, 42, (item) => item.age as number));
+console.log(lastIndexOf(ages, 42));
+console.log(lastIndexOf(users, 42, (item) => item.age as number));
+console.log(indexOf(ages, 100));
+console.log(lastIndexOf(ages, 100));
